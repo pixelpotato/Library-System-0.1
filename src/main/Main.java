046a72474d61e75f428a4d7a42f3ed8f49aa44da
Hamdan Javeed <hamdan@hamdanjaveed.com>
2013-03-13 14:32:40 -0400

    continued work on textview

--- src/main/Main.java
+++ src/main/Main.java
@@ -2,40 +2,52 @@ package main;
 
 import org.newdawn.slick.AppGameContainer;
 import org.newdawn.slick.SlickException;
+import org.newdawn.slick.util.Log;
 
-import controller.LibraryController;
+import controller.LibraryViewController;
 
 public class Main {
 	
 	private static final int DISPLAY_WIDTH; // width of the window
 	private static final int DISPLAY_HEIGHT; // height of the window
-	private static final boolean DISPLAY_IS_FULLSCREEN; // whether the program runs in full screen mode or not
-	private static final int DISPLAY_TARGET_FRAME_RATE; // the frame rate of the program
+	private static final boolean DISPLAY_IS_FULLSCREEN; // whether the program
+														// runs in full screen
+														// mode or not
+	private static final int DISPLAY_TARGET_FRAME_RATE; // the frame rate of the
+														// program
 	
 	static {
 		// the screen has an aspect ratio of 1280/720, which is about 1.67 : 1
 		DISPLAY_WIDTH = 1000; // 1000 pixels wide
 		DISPLAY_HEIGHT = 562; // 562 pixels in height
 		DISPLAY_IS_FULLSCREEN = false; // run in a window
-		DISPLAY_TARGET_FRAME_RATE = 60; // go through program loop 60 times a second
+		DISPLAY_TARGET_FRAME_RATE = 60; // go through program loop 60 times a
+										// second
 	}
 	
 	/**
-	 * The main method creates an AppGameContainer that holds the library program. A new instance of
-	 * core.LibraryController is created. The display mode of the AppGameContainer is set, as well as
-	 * the target frame rate. Finally, the library system is started and the program proceeds to the
+	 * The main method creates an AppGameContainer that holds the library
+	 * program. A new instance of core.LibraryController is created. The display
+	 * mode of the AppGameContainer is set, as well as the target frame rate.
+	 * Finally, the library system is started and the program proceeds to the
 	 * core.LibraryController instance's initStatesList() method.
-	 * @param args the command line arguments
+	 * 
+	 * @param args
+	 *            the command line arguments
 	 * */
 	public static void main(String[] args) throws SlickException {
-		// create new AppGameContainer, call the title of the window "Library System 0.1"
-		AppGameContainer gameContainer = new AppGameContainer(new LibraryController("Library System 0.1"));
+		// create new AppGameContainer, call the title of the window
+		// "Library System 0.1"
+		AppGameContainer gameContainer = new AppGameContainer(new LibraryViewController("Library System 0.1"));
 		// set the display mode of the window (width, height and full screen)
 		gameContainer.setDisplayMode(DISPLAY_WIDTH, DISPLAY_HEIGHT, DISPLAY_IS_FULLSCREEN);
 		// set the frame rate
 		gameContainer.setTargetFrameRate(DISPLAY_TARGET_FRAME_RATE);
+		// set verbose to false to the program doesn't print unnecessary info to
+		// the console
+		Log.setVerbose(false);
 		// start the program
 		gameContainer.start();
 	}
-
+	
 }
